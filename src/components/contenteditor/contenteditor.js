69d04d60697865f2a3719d79ce7eaf296030b4af
Sina Eetezadi <code@eetezadi.com>
2024-02-23 21:32:01 +0000

    feat(counter): new counter component
    Show remaining letters in message. Code restructering, linitng and documentation.

---
+++ src/components/contenteditor/contenteditor.js
@@ -0,0 +1,32 @@
+/**
+ * Calculates the remaining chars for Meta Threads
+ * Adds class "is-over" if over limit
+ * Content editor component for the WordPress block.
+ *
+ * @param {string} post - Post content.
+ * @param {string} textColor - Text color. Set in sidepanel.
+ * @param {Function} setAttributes - Function to set attributes.
+ * @return {JSX.Element} - Element to render.
+ */
+
+import { __ } from '@wordpress/i18n';
+import { RichText } from '@wordpress/block-editor';
+
+export default function ContentEditor( { post, textColor, setAttributes } ) {
+	const onChangePost = ( newPost ) => {
+		setAttributes( { post: newPost } );
+	};
+
+	return (
+		<RichText
+			tagName="p"
+			onChange={ onChangePost }
+			value={ post }
+			allowedFormats={ [] }
+			placeholder={ __( 'Write your Threads post…' ) }
+			style={ {
+				color: textColor,
+			} }
+		></RichText>
+	);
+}
---
+++ src/components/counter/counter.js
@@ -0,0 +1,27 @@
+/**
+ * Calculates the remaining chars for Meta Threads
+ * Adds class "is-over" if over limit
+ *
+ * @param {string} shareURL - Computed url for the Threads API
+ * @return {JSX.Element} Element to render.
+ */
+
+// CSS Styles
+import './counter.scss';
+
+export default function Counter( { shareURL } ) {
+	const maxChars = 500; //Threads limit
+	const countString = shareURL
+		? decodeURIComponent( shareURL.split( '=', 2 )[ 1 ] )
+		: ''; // shareString not initialized on first call
+	const remainingChars = maxChars - countString.length;
+
+	const defaulClass = 'wp-block-eetezadi-click2threads-counter';
+	const highlightClass = remainingChars < 0 ? ' is-over' : ''; // add highlight
+
+	return (
+		<span className={ `${ defaulClass }${ highlightClass }` }>
+			{ remainingChars }
+		</span>
+	);
+}
